f90a4e32b30d51be1e13b723c6db3703ee0c32bd
Rugved 💻 <rugveddanej.socialmedia@gmail.com>
2025-06-14 13:49:43 +0530

    Update certificates.ts

--- src/data/certificates.ts
+++ src/data/certificates.ts
@@ -1,7 +1,8 @@
 import { CertificateProps } from '../components/CertificateCard';
 import c1 from '../assets/certificates/www.freecodecamp.org_certification_rugveddanej_responsive-web-design.png';
-import c2 from '../assets/certificates/www.freecodecamp.org_certification_rugveddanej_front-end-development-libraries.png';
-import c3 from '../assets/certificates/www.freecodecamp.org_certification_rugveddanej_data-visualization.png';
+import c2 from '../assets/certificates/www.freecodecamp.org_certification_rugveddanej_javascript-algorithms-and-data-structures.png';
+import c3 from '../assets/certificates/www.freecodecamp.org_certification_rugveddanej_front-end-development-libraries.png';
+import c4 from '../assets/certificates/www.freecodecamp.org_certification_rugveddanej_data-visualization.png';
 
 const certificates: CertificateProps[] = [
   {
@@ -16,25 +17,37 @@ const certificates: CertificateProps[] = [
     hours: 300,
     verified: true
   },
-  {
+   {
     id: 2,
+    title: "Legacy JavaScript Algorithms and Data Structures",
+    issuer: "freeCodeCamp",
+    issueDate: "2025-06-13",
+    credentialUrl: "https://www.freecodecamp.org/certification/rugveddanej/javascript-algorithms-and-data-structures",
+    image: c2,
+    skills: ["JavaScript ES6+", "Algorithms", "Data Structures", "Object-Oriented Programming", "Functional Programming"],
+    description: "Mastering fundamental JavaScript concepts, including advanced algorithms, various data structures, and problem-solving techniques essential for complex programming challenges.",
+    hours: 300,
+    verified: true
+  },
+  {
+    id: 3,
     title: "Front End Development Libraries",
     issuer: "freeCodeCamp",
-    issueDate: "2025-06-13", // Example updated date
+    issueDate: "2025-06-13",
     credentialUrl: "https://www.freecodecamp.org/certification/rugveddanej/front-end-development-libraries",
-    image: c2,
+    image: c3,
     skills: ["React", "Redux", "Sass", "Bootstrap", "jQuery"],
     description: "Certification in building complex front-end applications using popular libraries and frameworks, focusing on React, Redux, and component-based architecture.",
     hours: 300,
     verified: true
   },
   {
-    id: 3,
+    id: 4,
     title: "Data Visualization",
     issuer: "freeCodeCamp",
-    issueDate: "2025-06-13", // Example updated date
+    issueDate: "2025-06-13",
     credentialUrl: "https://www.freecodecamp.org/certification/rugveddanej/data-visualization",
-    image: c3,
+    image: c4,
     skills: ["D3.js", "JSON", "APIs", "Data Structures", "SVG"],
     description: "Certification focused on representing data visually using D3.js and other tools, including working with APIs and various data formats.",
     hours: 300,
